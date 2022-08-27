725c835e809e275ee0ac7a3d178c61c4dc6171cc
dmeekins <drew.a.meekins@gmail.com>
2022-08-27 12:24:16 -0400

    setup my controllers and added my middleware
    Signed-off-by: dmeekins <drew.a.meekins@gmail.com>

--- server.js
+++ server.js
@@ -1,3 +1,34 @@
 // setup requirements
 const express = require('express')
 const app = express()
+const Profile = require('./models/profiles')
+const Location = require('./models/locations')
+const methodOverride = require('method-override')
+const profileController = require('./controllers/profileController')
+const locationController = require('./controllers/locationController')
+
+// View Setup
+app.set('view engine', 'ejs')
+
+// require .env file
+// const PORT = process.env.PORT
+const PORT = 3000
+const mongodbURI = process.env.MONGODBURI
+
+// middleware
+app.use((methodOverride('_method')))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static('static'))
+app.use(profileController)
+app.use(locationController)
+
+// index route
+app.get('/', (req, res) => {
+    res.send('Welcome To Crossroads')
+})
+
+// setup server
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+})
